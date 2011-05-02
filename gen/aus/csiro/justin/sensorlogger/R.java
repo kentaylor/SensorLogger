a53997baf02cfa10c291e4c316ecc1c6261e1d0b
Amir Hadad <amir@Silver.local>
2011-05-02 17:04:37 +1000

    Adding the location info and comment: started

--- gen/aus/csiro/justin/sensorlogger/R.java
+++ gen/aus/csiro/justin/sensorlogger/R.java
@@ -10,6 +10,7 @@ package aus.csiro.justin.sensorlogger;
 public final class R {
     public static final class array {
         public static final int activity_list=0x7f060000;
+        public static final int handset_location=0x7f060001;
     }
     public static final class attr {
     }
