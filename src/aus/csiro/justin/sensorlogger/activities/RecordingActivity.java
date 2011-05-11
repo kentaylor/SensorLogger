e9b2aa234f0d402323e931111d665603051c9288
Amir Hadad <amir.hadad@gmail.com>
2011-05-11 12:20:07 +1000

    Problem with timer in recording activity. Fixed by using system time instead of the old version that was using task delay and ordinery counting.

--- src/aus/csiro/justin/sensorlogger/activities/RecordingActivity.java
+++ src/aus/csiro/justin/sensorlogger/activities/RecordingActivity.java
@@ -47,11 +47,7 @@ public class RecordingActivity extends BoundActivity implements OnClickListener,
 	private float[] mOData = new float[3];
 	private float[] mR = new float[16];
 	private float[] mI = new float[16];
-	private FloatBuffer mVertexBuffer;
-	private FloatBuffer mColorBuffer;
-	private ByteBuffer mIndexBuffer;
-	private float[] mOrientation = new float[3];
-	private int mCount;
+	private long startTime;
 	private static final int DELAY = 30000;
 	int defTimeOut =0;
 	private final Handler handler = new Handler();
@@ -64,10 +60,34 @@ public class RecordingActivity extends BoundActivity implements OnClickListener,
 	private final TimerTask task = new TimerTask() {
 		@Override
 		public void run() {
+			handler.postDelayed(task, 1000);
 			checkSamples();
 		}
 	};
 
+	public String ElapsedTime()
+	{
+		long elapsedTime = System.currentTimeMillis() - startTime;
+	      elapsedTime = elapsedTime / 1000;
+
+	      String seconds = Integer.toString((int) (elapsedTime % 60));
+	      String minutes = Integer.toString((int) ((elapsedTime % 3600) / 60));
+	      String hours = Integer.toString((int) (elapsedTime / 3600));
+
+	      if (seconds.length() < 2) {
+	        seconds = "0" + seconds;
+	      }
+
+	      if (minutes.length() < 2) {
+	        minutes = "0" + minutes;
+	      }
+
+	      if (hours.length() < 2) {
+	        hours = "0" + hours;
+	      }
+	      return hours + ":" + minutes + ":" + seconds;
+
+	}
 	//When "Stop" button is clicked, 
 	public void onClick(View arg0) {
 		FlurryAgent.onEvent("process_stop_click");
@@ -102,92 +122,13 @@ public class RecordingActivity extends BoundActivity implements OnClickListener,
 
 		sensors = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
-
+		startTime = System.currentTimeMillis();
+		
 		handler.removeCallbacks(task);
 		handler.postDelayed(task, 500);
 
 	}
-	//--------------------------testing for how the data is collecting well---------------------
-	//-----------------------------mainly just display data on the screen-----------------------
-	//    public void onSensorChanged(SensorEvent event) {
-	//    	TextView status = (TextView) findViewById(R.id.status);
-	//    	TextView status1 = (TextView) findViewById(R.id.status_1);
-	//        float[] data;
-	//
-	//        String allSensor = "Sensor = \n";
-	//        String allSensor1 = "\nSensor = \n";
-	//        
-	//        switch (event.sensor.getType()) {
-	//        case Sensor.TYPE_ACCELEROMETER:
-	//        	data = mGData;
-	//        	accels = event.values.clone();
-	//        	float value;
-	//        	
-	//            for (int i=0;i<3;i++) {
-	//            	value=event.values[i];
-	//            	
-	//                allSensor += "accel val = " + value + "\n";
-	//            }
-	//
-	//            break;
-	// 
-	//        case Sensor.TYPE_MAGNETIC_FIELD:
-	//        	data = mMData;
-	//        	mags = event.values.clone();
-	//            for (int i=0;i<3;i++) {
-	//            	value=event.values[i];
-	//            	
-	//                allSensor += "magnetic val = " + value + "\n";
-	//            }
-	//
-	//            break;
-	//
-	//
-	//        }
-	//
-	//        status.setText(allSensor);
-	//        if(mags != null && accels != null){
-	//        SensorManager.getRotationMatrix(mR, mI, accels, mags);
-	//        allSensor1=(float)(mR[0]) +" " + (float)(mR[1]) +" " + (float)(mR[2]) +" " + (float)(mR[3]) +"\n"+
-	//		(float)(mR[4]) +" " + (float)(mR[5]) +" " + (float)(mR[6]) +" " + (float)(mR[7]) +"\n"+
-	//		(float)(mR[8]) +" " + (float)(mR[9]) +" " + (float)(mR[10]) +" " + (float)(mR[11]) +"\n"+
-	//		(float)(mR[12]) +" " + (float)(mR[13]) +" " + (float)(mR[14]) +" " + (float)(mR[15]) +"\n";
-	////        allSensor1+=(int)(mI[0]) +" " + (int)(mI[1]) +" " + (int)(mI[2]) +" " + (int)(mI[3]) +"\n"+
-	////		(int)(mI[4]) +" " + (int)(mI[5]) +" " + (int)(mI[6]) +" " + (int)(mI[7]) +"\n"+
-	////		(int)(mI[8]) +" " + (int)(mI[9]) +" " + (int)(mI[10]) +" " + (int)(mI[11]) +"\n"+
-	////		(int)(mI[12]) +" " + (int)(mI[13]) +" " + (int)(mI[14]) +" " + (int)(mI[15]) +"\n";
-	//
-	//        SensorManager.getOrientation(mR, mOrientation);
-	//        float incl = SensorManager.getInclination(mI);
-	////        mGData[0]=mR[0]*accels[0]+mR[1]*accels[1]+mR[2]*accels[2];
-	////        if(mGData[0]<0.00001 && mGData[0]>0 || mGData[0]>-0.00001 && mGData[0]<0) mGData[0]=0.0f;
-	////        mGData[1]=mR[4]*accels[0]+mR[5]*accels[1]+mR[6]*accels[2];
-	////        if(mGData[1]<0.00001 && mGData[1]>0 || mGData[0]>-0.00001 && mGData[0]<0) mGData[1]=0.0f;
-	////        mGData[2]=mR[8]*accels[0]+mR[9]*accels[1]+mR[10]*accels[2];
-	////        if(mGData[2]<0.00001 && mGData[2]>0 || mGData[0]>-0.00001 && mGData[0]<0) mGData[2]=0.0f;
-	////        allSensor1 += "accel val = " + mGData[0] + "\n";
-	////        allSensor1 += "accel val = " + mGData[1] + "\n";
-	////        allSensor1 += "accel val = " + mGData[2] + "\n";
-	//        
-	//            final float rad2deg = (float)(180.0f/Math.PI);
-	//            mCount = 0;
-	//            allSensor1="After rotating: =\n"+"yaw: " + (int)(mOrientation[0]*rad2deg) +"\n"+
-	//                    "  pitch: " + (int)(mOrientation[1]*rad2deg) +"\n"+
-	//                    "  roll: " + (int)(mOrientation[2]*rad2deg) +"\n"+
-	//                    "  incl: " + (int)(incl*rad2deg)+"\n";
-	//////                    
-	////            Log.d("Compass", "yaw: " + (int)(mOrientation[0]*rad2deg) +
-	////                    "  pitch: " + (int)(mOrientation[1]*rad2deg) +
-	////                    "  roll: " + (int)(mOrientation[2]*rad2deg) +
-	////                    "  incl: " + (int)(incl*rad2deg)
-	////                    );
-	//            status1.setText(allSensor1);
-	//        }else{
-	//        	status1.setText("nothing");
-	//        }
-	//        
-	//    }
-
+	
 	public void onPause() {
 		if (sensors != null) {
 			sensors.unregisterListener(this);
@@ -209,31 +150,8 @@ public class RecordingActivity extends BoundActivity implements OnClickListener,
 
 	public void checkSamples() {
 		try {
-			phase = (phase + 1) % 4;
-			check = (check+1) % 2;
-			String time = "";
-			String text = "?";
-			switch (phase) {
-			case 0: text = ".  "; break;
-			case 1: text = " . "; break;
-			case 2: text = "  ."; break;
-			case 3: text = " . "; break;
-			}
-			switch(check){
-			case 0: break;
-			case 1: sec++;
-			if(sec==60){
-				sec=0;
-				min++;
-			}
-			if(min==60){
-				min=0;
-				hour++;
-			}
-			break;
-
-			}
-			time = "0"+hour+" : "+min+" : "+sec;
+			
+			String time = ElapsedTime();
 
 			((TextView) findViewById(R.id.recordingcount)).setText(time);
 
@@ -260,9 +178,7 @@ public class RecordingActivity extends BoundActivity implements OnClickListener,
 				FlurryAgent.onEvent("countdown_to_results");
 				startActivity(new Intent(this, ResultsActivity.class));
 				finish();
-			} else {
-				handler.postDelayed(task, 500);
-			}
+			} 
 		} catch (RemoteException ex) {
 			Log.e(getClass().getName(), "Error getting countdown", ex);
 		}
